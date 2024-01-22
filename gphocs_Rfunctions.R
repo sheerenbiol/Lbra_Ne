45dbc2a857e08ce49290c354428abfbdad597a62
sheerenbiol <sheeren@itg.be>
2024-01-22 12:24:12 +0100

    altered first functions + added some new functinos.

--- gphocs_Rfunctions.R
+++ gphocs_Rfunctions.R
@@ -4,20 +4,23 @@
 #####            Genomic population structure        #####
 #####                    South America               #####
 ##########################################################
-##### GOAL: FUNCTIONS for XXX.R
+
+### Packages:
+library(tracerer); library(tidyr)
+library(ggplot2)
+
+#####################
+##### FUNCTIONS #####
+#####################
 
 ### Calculate ESS:
 ##################
 calc_ess <- function(mig.mod = c('No', 'AM2ATL', 'ATL2AM','BiDir'), sub_sample = c('1_Subset1','2_Subset2','3_Subset3')){
-  print(" Function to Calculate the ESS (effective Sample size) for each G-PhoCS run for a particular sample subset.
-  IMPORTANT: empty log files should be removed from the log-file directory - otherwise the function will give an error message
-
-  INPUT:
-  mig.mod     - Migration model that was used OPTIONS: No, AM2ATL, ATL2AM, BiDir
-  sub_sample  - certain sub-sample of individuals that was used in the G-PhoCS run
-  ")
-  ## Required package:
-  library(tracerer)
+  print(" Function to Calculate the ESS (effective Sample size) for each G-PhoCS run for a particular sample subset.")
+  print("IMPORTANT: empty log files should be removed from the log-file directory - otherwise the function will give an error message")
+  print("INPUT:")
+  print("mig.mod     - Migration model that was used OPTIONS: No, AM2ATL, ATL2AM, BiDir")
+  print("sub_sample  - certain sub-sample of individuals that was used in the G-PhoCS run")
 
   ## Set paths for the respective model that was used
   if(mig.mod == 'No') {
@@ -102,15 +105,13 @@ calc_ess <- function(mig.mod = c('No', 'AM2ATL', 'ATL2AM','BiDir'), sub_sample =
 calc_Ne_from_theta <- function(mig.mod = c('No', 'AM2ATL', 'ATL2AM','BiDir'),
                                sub_sample = c('1_Subset1','2_Subset2','3_Subset3'),
                                chrs = NULL){
-  print(" Calculating the Ne from the estimated theta values using Ne = theta / (4* 1.99e-09).
-  IMPORTANT: empty log files should be removed from the log-file directory - otherwise the function will give an error message.
-  IMPORTANT: !! Only select chromosomes that show ESS > 200 for all inferred theta values!!
-
-INPUT:
-  mig.mod     - Migration model that was used OPTIONS: No, AM2ATL, ATL2AM, BiDir
-  sub_sample  - certain sub-sample of individuals that was used in the G-PhoCS run
-  chrs        - vector of chromosome with ESS > 200 for all theta values.
-  ")
+  print(" Calculating the Ne from the estimated theta values using Ne = theta / (4* 1.99e-09).")
+  print("IMPORTANT: empty log files should be removed from the log-file directory - otherwise the function will give an error message.")
+  print("IMPORTANT: !! Only select chromosomes that show ESS > 200 for all inferred theta values!!")
+  print("INPUT:")
+  print("mig.mod     - Migration model that was used OPTIONS: No, AM2ATL, ATL2AM, BiDir")
+  print("sub_sample  - certain sub-sample of individuals that was used in the G-PhoCS run")
+  print("chrs        - vector of chromosome with ESS > 200 for all theta values.")
 
   ## Set paths for the respective model that was used
   if(mig.mod == 'No') {
@@ -235,12 +236,69 @@ INPUT:
 
 ### Plot Ne per sample sub-set:
 ###############################
+Neplot_subset <- function(theta.data = NULL){
+  print("Function to plot the estimated Ne for a certain model for a certain sample subset")
+  print("INPUT:")
+  print("theta.data - theta dataframe in long format. output of calc_Ne_from_theta")
 
-### Plot Ne with all subsets - facetted:
-########################################
+  ## Set the order:
+  level_order <- c("theta_WAM", "theta_CAM", "theta_AM" , "theta_ATL")
+  ## Plot:
+  subset_plot <- ggplot(theta.data, aes(x=factor(Ne_pop, level = level_order), y=Ne_value)) +
+    geom_violin(data=theta.data, aes(col=Ne_pop, fill=Ne_pop), alpha=0.2, width=0.7)+
+    scale_fill_manual(values = c("#A2662C", "#4E87B8", "#723957", "gray50")) +
+    geom_jitter(data=theta.data, aes(col = Ne_pop ), alpha=0.9, width=0.15, size = 3, stroke=1, shape=16) +
+    scale_color_manual(values = c("#A2662C", "#4E87B8", "#723957", "gray50")) +
+    geom_point(stat="summary", fun.y="mean_se", size = 3, shape=21, stroke = 1.5) +
+    geom_errorbar(stat="summary", fun.data="mean_se", fun.args = list(mult = 1.96), width=0, linewidth=1) +
+    labs(x="Populations", y="Effective population size (No. inds.)") +
+    theme_bw() +
+    theme(axis.text.x=element_text(size=10,angle=0, hjust=0.5))
 
-### Plot Ne with all subsets - averaged:
+  return(subset_plot)
+}
+
+### Plot Ne with all subsets - facetted/averaged:
 ########################################
+Neplot_all <- function(theta.data.combined = NULL, plot.fmt = c('facet','average')){
+  print("Function to plot the estimated Ne for a certain model for a certain sample subset")
+  print("INPUT:")
+  print("theta.data.combined - theta dataframe in long format. output of calc_Ne_from_theta")
+
+  ## Set the order:
+  level_order <- c("theta_WAM", "theta_CAM", "theta_AM" , "theta_ATL")
+
+  ## Facet Plot:
+  if (plot.fmt == 'facet'){
+    facet_plot <- ggplot(theta.data.combined, aes(x=sbset, y=Ne_value)) +
+      geom_violin(aes(color = Ne_pop, fill=Ne_pop), alpha=0.2)+
+      scale_fill_manual(values =  c("#A2662C", "#4E87B8", "#723957", "gray50")) +
+      facet_wrap(~ factor(Ne_pop, level = level_order), ncol=4) +
+      scale_color_manual(values = c("#A2662C", "#4E87B8", "#723957", "gray50"))+
+      geom_jitter(aes(color = Ne_pop ), alpha=0.8, width=0.2, size = 3, stroke=1, shape=16) +
+      geom_point(stat="summary", fun.y="mean_se", size = 3, shape=21, stroke = 1.5, position = position_dodge2(.5)) +
+      geom_errorbar(stat="summary", fun.data="mean_se", fun.args = list(mult = 1.96), width=0, linewidth=1, position = position_dodge2(.5)) +
+      labs(x="Populations", y="Effective population size (No. inds.)") +
+      theme_bw() +
+      theme(axis.text.x=element_text(size=10,angle=0, hjust=0.5))
+    return(facet_plot)
+
+    ## Averaged plot:
+  } else if (plot.fmt == 'average'){
+    average_plot <- ggplot(theta.data.combined, aes(x=factor(Ne_pop, level = level_order), y=Ne_value)) +
+      geom_violin(aes(color = Ne_pop, fill=Ne_pop), alpha=0.2)+
+      scale_fill_manual(values =  c("#A2662C", "#4E87B8", "#723957", "gray50")) +
+      scale_color_manual(values = c("#A2662C", "#4E87B8", "#723957", "gray50"))+
+      geom_jitter(aes(color = Ne_pop ), alpha=0.8, width=0.2, size = 3, stroke=1, shape=16) +
+      geom_point(stat="summary", fun.y="mean_se", size = 3, shape=21, stroke = 1.5, position = position_dodge2(.5)) +
+      geom_errorbar(stat="summary", fun.data="mean_se", fun.args = list(mult = 1.96), width=0, linewidth=1, position = position_dodge2(.5)) +
+      labs(x="Populations", y="Effective population size (No. inds.)") +
+      theme_bw() +
+      theme(axis.text.x=element_text(size=10,angle=0, hjust=0.5))
+    return(average_plot)
+  }
+}
+
 
 ### Create summary table - mean Ne + std error:
 ###############################################
