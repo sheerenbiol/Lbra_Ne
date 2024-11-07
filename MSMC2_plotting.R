783d1d548883ec396c3a9b929553f9dfe5b7f29c
sheerenbiol <sheeren@itg.be>
2024-11-07 12:18:11 +0100

    Changes to Rhistory and msmc2 script + addinf msmc2 rsession stuff

--- MSMC2_plotting.R
+++ MSMC2_plotting.R
@@ -6,17 +6,17 @@
 ##########################################################
 
 ### Packages:
-library(ggplot2);library(ggforce)
+library(ggplot2);library(ggforce); library(ggpubr)
 
 ### Checking MSMC results for all-population-in-one input file:
 ###############################################################
 ### Checking Log-Likelihood:
-log.files <- list.files('~/Dropbox/Projects/Lbraz_genome_paper/Lbraziliensis/3_LBRA/2_Popgen/2_Popgen_with_PER186/10_G-PhoCS/4_MSMC2/3_msmc2_output/3_AllSNPs_EM500/', pattern = 'EM500.MSMC2.out.loop.txt')
+log.files <- list.files('./MSMC2_Data/', pattern = 'EM500.MSMC2.out.loop.txt')
 
 ## --> Showing that Log Likelihood is stable at 1000 iterations
 par(mfrow=c(3,3))
 for (file in 1:length(log.files)){
-  tmp <- read.table(paste0('~/Dropbox/Projects/Lbraz_genome_paper/Lbraziliensis/3_LBRA/2_Popgen/2_Popgen_with_PER186/10_G-PhoCS/4_MSMC2/3_msmc2_output/3_AllSNPs_EM500/',log.files[[file]]), header = F)
+  tmp <- read.table(paste0('./MSMC2_Data/',log.files[[file]]), header = F)
   plot(x=rownames(tmp), y=tmp$V2,
        #main=paste0(log.files[[file]]),
        xlab='EM Iterations', ylab='Log Likelihood', main=paste0(log.files[[file]]),
@@ -25,22 +25,22 @@ for (file in 1:length(log.files)){
 par(mfrow=c(1,1))
 
 ### Data visualization
-data.files <- list.files('~/Dropbox/Projects/Lbraz_genome_paper/Lbraziliensis/3_LBRA/2_Popgen/2_Popgen_with_PER186/10_G-PhoCS/4_MSMC2/3_msmc2_output/3_AllSNPs_EM500/', pattern = 'EM500.MSMC2.out.final.txt')
+data.files <- list.files('./MSMC2_Data/', pattern = 'EM500.MSMC2.out.final.txt')
 data.combined <- list()
-for ( file in 1:9){ #1:length(data.files)
-  tmp <- read.table(paste0('~/Dropbox/Projects/Lbraz_genome_paper/Lbraziliensis/3_LBRA/2_Popgen/2_Popgen_with_PER186/10_G-PhoCS/4_MSMC2/3_msmc2_output/3_AllSNPs_EM500/',data.files[[file]]), header = T)
-  if (file == 1) { tmp$pop <- c(rep('ATL-1',nrow(tmp))); tmp$pop2 <- c(rep('ATL',nrow(tmp)))}
-  else if (file == 2) {tmp$pop <- c(rep('ATL-2',nrow(tmp))); tmp$pop2 <- c(rep('ATL',nrow(tmp)))}
-  else if (file == 3) {tmp$pop <- c(rep('ATL-3',nrow(tmp))); tmp$pop2 <- c(rep('ATL',nrow(tmp)))}
-  else if (file == 4) {tmp$pop <- c(rep('CAM-1',nrow(tmp))); tmp$pop2 <- c(rep('CAM',nrow(tmp)))}
-  else if (file == 5) {tmp$pop <- c(rep('CAM-2',nrow(tmp))); tmp$pop2 <- c(rep('CAM',nrow(tmp)))}
-  else if (file == 6) {tmp$pop <- c(rep('CAM-3',nrow(tmp))); tmp$pop2 <- c(rep('CAM',nrow(tmp)))}
-  else if (file == 7) {tmp$pop <- c(rep('WAM-1',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
-  else if (file == 8) {tmp$pop <- c(rep('WAM-2',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
-  else if (file == 9) {tmp$pop <- c(rep('WAM-3',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
-  #else if (file == 10) {tmp$pop <- c(rep('WAM-1',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
-  #else if (file == 11) {tmp$pop <- c(rep('WAM-2',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
-  #else if (file == 12) {tmp$pop <- c(rep('WAM-3',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
+for ( file in 1:12){ #1:length(data.files)
+  tmp <- read.table(paste0('./MSMC2_Data/',data.files[[file]]), header = T)
+  if (file == 1) { tmp$pop <- c(rep('AM-1',nrow(tmp))); tmp$pop2 <- c(rep('AM',nrow(tmp)))}
+  else if (file == 2) {tmp$pop <- c(rep('AM-2',nrow(tmp))); tmp$pop2 <- c(rep('AM',nrow(tmp)))}
+  else if (file == 3) {tmp$pop <- c(rep('AM-3',nrow(tmp))); tmp$pop2 <- c(rep('AM',nrow(tmp)))}
+  else if (file == 4) {tmp$pop <- c(rep('ATL-1',nrow(tmp))); tmp$pop2 <- c(rep('ATL',nrow(tmp)))}
+  else if (file == 5) {tmp$pop <- c(rep('ATL-2',nrow(tmp))); tmp$pop2 <- c(rep('ATL',nrow(tmp)))}
+  else if (file == 6) {tmp$pop <- c(rep('ATL-3',nrow(tmp))); tmp$pop2 <- c(rep('ATL',nrow(tmp)))}
+  else if (file == 7) {tmp$pop <- c(rep('CAM-1',nrow(tmp))); tmp$pop2 <- c(rep('CAM',nrow(tmp)))}
+  else if (file == 8) {tmp$pop <- c(rep('CAM-2',nrow(tmp))); tmp$pop2 <- c(rep('CAM',nrow(tmp)))}
+  else if (file == 9) {tmp$pop <- c(rep('CAM-3',nrow(tmp))); tmp$pop2 <- c(rep('CAM',nrow(tmp)))}
+  else if (file == 10) {tmp$pop <- c(rep('WAM-1',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
+  else if (file == 11) {tmp$pop <- c(rep('WAM-2',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
+  else if (file == 12) {tmp$pop <- c(rep('WAM-3',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
 
   data.combined[[file]] <- tmp
 }
@@ -90,15 +90,6 @@ pop.size <- (1/data.combined$lambda)/(2*mu)
 data.combined$popsize <- pop.size
 
 
-ggplot(data=data.combined[which(data.combined$time <= 1e07),], aes(x=(time), y=log10(popsize), fill=pop2, col=pop2))+
-  #geom_line(linewidth = 1.5, alpha=0.75) +
-  geom_smooth(method = "loess",formula = y~x, se = F, span = 0.3 ) +
-  scale_color_manual(values = c("gray50","#723957","#4E87B8","#A2662C","#4E87B8","#4E87B8","#A2662C","#A2662C", "#A2662C","gray50","gray50","gray50"))+
-  stat_cor(method = "pearson", label.x = 3, label.y = 8)+
-  stat_summary_bin(fun.data='mean_cl_boot', size=0.75)+
-  xlab('No. generations') + ylab('Log. Effective population size') #+
-#xlim(-1,8)
-
 ggplot(data=subset(data.combined, data.combined$pop2 != 'AM'), aes(x=(time), y=log10(popsize), fill=pop, col=pop))+
   #geom_line(linewidth = 1.5, alpha=0.75) +
   geom_step(subset(data.combined, data.combined$pop2 != 'AM'), mapping=aes(x=time, y=log10(popsize)), direction = 'hv', linetype=1, linewidth=1.5)+
@@ -115,8 +106,8 @@ ggplot(data=subset(data.combined, data.combined$pop2 != 'AM'), aes(x=(time), y=l
 ggplot(data=subset(data.combined, data.combined$pop2 != 'AM')[which(subset(data.combined, data.combined$pop2 != 'AM')$time <= 2.5e07),], aes(x=(time), y=log10(popsize), fill=pop, col=pop))+
   #geom_line(linewidth = 1.5, alpha=0.75) +
   geom_step(subset(data.combined, data.combined$pop2 != 'AM')[which(subset(data.combined, data.combined$pop2 != 'AM')$time <= 2.5e07),], mapping=aes(x=time, y=log10(popsize)), direction = 'hv', linetype=1, linewidth=1.5)+
-  scale_color_manual(values = c("#723957","#723957","#723957","#4E87B8","#4E87B8","#4E87B8","#A2662C","#A2662C", "#A2662C","gray50","gray50","gray50"))+
-  #scale_color_manual(values = c("#723957","#4E87B8","#A2662C","#4E87B8","#4E87B8","#A2662C","#A2662C", "#A2662C","gray50","gray50","gray50"))+
+  #scale_color_manual(values = c("#723957","#723957","#723957","#4E87B8","#4E87B8","#4E87B8","#A2662C","#A2662C", "#A2662C","gray50","gray50","gray50"))+
+  scale_color_manual(values = c("#723957","#4E87B8","#A2662C","#4E87B8","#4E87B8","#4E87B8","#A2662C","#A2662C", "#A2662C","gray50","gray50","gray50"))+
   #stat_summary_bin(fun.data='mean_cl_boot', size=0.5)+
   xlab('No. generations') + ylab('Log. Effective population size') +
   theme_bw() +
@@ -128,131 +119,15 @@ ggplot(data=subset(data.combined, data.combined$pop2 != 'AM')[which(subset(data.
 facet_zoom(xlim = c(0,5e05))
 #xlim(-1,8)
 
-#######=========================================================================
-### Checking MSMC results for population-separate input files:
-##############################################################
-### Checking Log-Likelihood:
-#data <- read.table('~/Dropbox/Projects/Lbraz_genome_paper/Lbraziliensis/3_LBRA/2_Popgen/2_Popgen_with_PER186/10_G-PhoCS/4_MSMC2/3_msmc2_output/Pop_smallgenomeP/ATL.subset3.ALLsmallgen.EM1000.MSMC2.out.loop.txt', header = F)
-#head(data)
-#plot(x=rownames(data), y=data$V2)
-log.files <- list.files('~/Dropbox/Projects/Lbraz_genome_paper/Lbraziliensis/3_LBRA/2_Popgen/2_Popgen_with_PER186/10_G-PhoCS/4_MSMC2/3_msmc2_output/Pop_smallgenomeP/', pattern = 'EM1000_MSMC2_out.loop.txt')
-
-## --> Showing that Log Likelihood is stabel at 1000 iterationns
-par(mfrow=c(3,3))
-for (file in 1:length(log.files)){
-  tmp <- read.table(paste0('~/Dropbox/Projects/Lbraz_genome_paper/Lbraziliensis/3_LBRA/2_Popgen/2_Popgen_with_PER186/10_G-PhoCS/4_MSMC2/3_msmc2_output/Pop_smallgenomeP/',log.files[[file]]), header = F)
-  plot(x=rownames(tmp), y=tmp$V2,
-       #main=paste0(log.files[[file]]),
-       xlab='EM Iterations', ylab='Log Likelihood',
-       xlim=c(0,1000), ylim=c(min(tmp$V2)-1000,max(tmp$V2)+1000))
-}
-par(mfrow=c(1,1))
-
-### Data visualization
-data.files <- list.files('~/Dropbox/Projects/Lbraz_genome_paper/Lbraziliensis/3_LBRA/2_Popgen/2_Popgen_with_PER186/10_G-PhoCS/4_MSMC2/3_msmc2_output/Pop_smallgenomeP/', pattern = 'EM1000_MSMC2_out.final.txt')
-data.combined <- list()
-for ( file in 1:length(data.files)){
-  tmp <- read.table(paste0('~/Dropbox/Projects/Lbraz_genome_paper/Lbraziliensis/3_LBRA/2_Popgen/2_Popgen_with_PER186/10_G-PhoCS/4_MSMC2/3_msmc2_output/Pop_smallgenomeP/',data.files[[file]]), header = T)
-  if (file == 1) { tmp$pop <- c(rep('ATL-1',nrow(tmp))); tmp$pop2 <- c(rep('ATL',nrow(tmp)))}
-  else if (file == 2) {tmp$pop <- c(rep('ATL-2',nrow(tmp))); tmp$pop2 <- c(rep('ATL',nrow(tmp)))}
-  else if (file == 3) {tmp$pop <- c(rep('ATL-3',nrow(tmp))); tmp$pop2 <- c(rep('ATL',nrow(tmp)))}
-  else if (file == 4) {tmp$pop <- c(rep('CAM-1',nrow(tmp))); tmp$pop2 <- c(rep('CAM',nrow(tmp)))}
-  else if (file == 5) {tmp$pop <- c(rep('CAM-2',nrow(tmp))); tmp$pop2 <- c(rep('CAM',nrow(tmp)))}
-  else if (file == 6) {tmp$pop <- c(rep('CAM-3',nrow(tmp))); tmp$pop2 <- c(rep('CAM',nrow(tmp)))}
-  else if (file == 7) {tmp$pop <- c(rep('WAM-1',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
-  else if (file == 8) {tmp$pop <- c(rep('WAM-2',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
-  else if (file == 9) {tmp$pop <- c(rep('WAM-3',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
-
-  data.combined[[file]] <- tmp
-}
-
-mu <- 1.99e-09 #unit bp per generation
-
-data.combined <- do.call(rbind, data.combined)
-
-time <- (data.combined$left_time_boundary/mu)
-data.combined$time <- time
-pop.size <- (1/data.combined$lambda)/(2*mu)
-data.combined$popsize <- pop.size
-
-ggplot(data=subset(data.combined, data.combined$pop2 != 'AM')[which(subset(data.combined, data.combined$pop2 != 'AM')$time <= 1e07),], aes(x=(time), y=log10(popsize), fill=pop2, col=pop2))+
-  #geom_line(linewidth = 1.5, alpha=0.75) +
-  geom_step(subset(data.combined, data.combined$pop2 != 'AM')[which(subset(data.combined, data.combined$pop2 != 'AM')$time <= 1e07),], mapping=aes(x=time, y=log10(popsize)), direction = 'hv', linetype=1, linewidth=1.5)+
-  #scale_color_manual(values = c("#723957","#723957","#723957","#4E87B8","#4E87B8","#4E87B8","#A2662C","#A2662C", "#A2662C","gray50","gray50","gray50"))+
-  scale_color_manual(values = c("#723957","#4E87B8","#A2662C","#4E87B8","#4E87B8","#A2662C","#A2662C", "#A2662C","gray50","gray50","gray50"))+
-  #stat_summary_bin(fun.data='mean_cl_boot', size=0.5)+
-  xlab('No. generations') + ylab('Log. Effective population size') +
-  theme_bw() +
-  theme(axis.text.x=element_text(size=10,angle=0, hjust=0.5)) +
-#facet_zoom(xlim=c(0,1e07))
-facet_zoom(xlim = c(0,1.25e06))
-#facet_zoom(xlim = c(1.25e06,5e06))
-#facet_zoom(xlim = c(5e06,10e06))
-#facet_zoom(xlim = c(0,1e06))
-#xlim(-1,8)
-
-
-### Data visualization
-data.files <- list.files('~/Dropbox/Projects/Lbraz_genome_paper/Lbraziliensis/3_LBRA/2_Popgen/2_Popgen_with_PER186/10_G-PhoCS/4_MSMC2/3_msmc2_output/', recursive = T, pattern = '.final.txt')
-data.files <- data.files[-c(1:3,13:18)]
-data.combined <- list()
-for ( file in 1:length(data.files)){
-  tmp <- read.table(paste0('~/Dropbox/Projects/Lbraz_genome_paper/Lbraziliensis/3_LBRA/2_Popgen/2_Popgen_with_PER186/10_G-PhoCS/4_MSMC2/3_msmc2_output/',data.files[[file]]), header = T)
-  if (file == 1) { tmp$pop <- c(rep('ATL-1',nrow(tmp))); tmp$pop2 <- c(rep('ATL',nrow(tmp)))}
-  else if (file == 2) {tmp$pop <- c(rep('ATL-2',nrow(tmp))); tmp$pop2 <- c(rep('ATL',nrow(tmp)))}
-  else if (file == 3) {tmp$pop <- c(rep('ATL-3',nrow(tmp))); tmp$pop2 <- c(rep('ATL',nrow(tmp)))}
-  else if (file == 4) {tmp$pop <- c(rep('CAM-1',nrow(tmp))); tmp$pop2 <- c(rep('CAM',nrow(tmp)))}
-  else if (file == 5) {tmp$pop <- c(rep('CAM-2',nrow(tmp))); tmp$pop2 <- c(rep('CAM',nrow(tmp)))}
-  else if (file == 6) {tmp$pop <- c(rep('CAM-3',nrow(tmp))); tmp$pop2 <- c(rep('CAM',nrow(tmp)))}
-  else if (file == 7) {tmp$pop <- c(rep('WAM-1',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
-  else if (file == 8) {tmp$pop <- c(rep('WAM-2',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
-  else if (file == 9) {tmp$pop <- c(rep('WAM-3',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
-  else if (file == 10) { tmp$pop <- c(rep('ATL-4',nrow(tmp))); tmp$pop2 <- c(rep('ATL',nrow(tmp)))}
-  else if (file == 11) {tmp$pop <- c(rep('ATL-5',nrow(tmp))); tmp$pop2 <- c(rep('ATL',nrow(tmp)))}
-  else if (file == 12) {tmp$pop <- c(rep('ATL-6',nrow(tmp))); tmp$pop2 <- c(rep('ATL',nrow(tmp)))}
-  else if (file == 13) {tmp$pop <- c(rep('CAM-4',nrow(tmp))); tmp$pop2 <- c(rep('CAM',nrow(tmp)))}
-  else if (file == 14) {tmp$pop <- c(rep('CAM-5',nrow(tmp))); tmp$pop2 <- c(rep('CAM',nrow(tmp)))}
-  else if (file == 15) {tmp$pop <- c(rep('CAM-6',nrow(tmp))); tmp$pop2 <- c(rep('CAM',nrow(tmp)))}
-  else if (file == 16) {tmp$pop <- c(rep('WAM-4',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
-  else if (file == 17) {tmp$pop <- c(rep('WAM-5',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
-  else if (file == 18) {tmp$pop <- c(rep('WAM-6',nrow(tmp))); tmp$pop2 <- c(rep('WAM',nrow(tmp)))}
-
-  data.combined[[file]] <- tmp
-}
-
-mu <- 1.99e-09 #unit bp per generation
-
-data.combined <- do.call(rbind, data.combined)
-
-time <- (data.combined$left_time_boundary/mu)
-data.combined$time <- time
-pop.size <- (1/data.combined$lambda)/(2*mu)
-data.combined$popsize <- pop.size
-
-ggplot(data=subset(data.combined, data.combined$pop2 != 'AM')[which(subset(data.combined, data.combined$pop2 != 'AM')$time <= 1e07),], aes(x=(time), y=log10(popsize), fill=pop, col=pop))+
-  #geom_line(linewidth = 1.5, alpha=0.75) +
-  geom_step(subset(data.combined, data.combined$pop2 != 'AM')[which(subset(data.combined, data.combined$pop2 != 'AM')$time <= 1e07),], mapping=aes(x=time, y=log10(popsize)), direction = 'hv', linetype=1, linewidth=1.5)+
-  scale_color_manual(values = c("#723957","#723957","#723957","#723957","#723957","#723957","#4E87B8","#4E87B8","#4E87B8","#4E87B8","#4E87B8","#4E87B8","#A2662C","#A2662C", "#A2662C","#A2662C","#A2662C", "#A2662C"))+
-  #scale_color_manual(values = c("#723957","#4E87B8","#A2662C","#4E87B8","#4E87B8","#A2662C","#A2662C", "#A2662C","gray50","gray50","gray50"))+
-  #stat_summary_bin(fun.data='mean_cl_boot', size=0.5)+
-  xlab('No. generations') + ylab('Log. Effective population size') +
-  theme_bw() +
-  theme(axis.text.x=element_text(size=10,angle=0, hjust=0.5)) +
-  #facet_zoom(xlim=c(0,1e07))
-  facet_zoom(xlim = c(0,1.5e06))
-#facet_zoom(xlim = c(1.25e06,5e06))
-#facet_zoom(xlim = c(5e06,10e06))
-#facet_zoom(xlim = c(0,1e06))
-#xlim(-1,8)
 
 ##############################
 ### Cross-Population analyses:
 ##############################
 ## WAM-CAM
-wam.cam <- list.files('~/Dropbox/Projects/Lbraz_genome_paper/Lbraziliensis/3_LBRA/2_Popgen/2_Popgen_with_PER186/10_G-PhoCS/4_MSMC2/3_msmc2_output/3_AllSNPs_EM500/CrossPopulation/', pattern = 'combined_WAM-CAM')
+wam.cam <- list.files('./MSMC2_Data/CrossPopulation/', pattern = 'combined_WAM-CAM')
 wam.cam.combined <- list()
 for ( file in 1:length(wam.cam)){
-  tmp <- read.table(paste0('~/Dropbox/Projects/Lbraz_genome_paper/Lbraziliensis/3_LBRA/2_Popgen/2_Popgen_with_PER186/10_G-PhoCS/4_MSMC2/3_msmc2_output/3_AllSNPs_EM500/CrossPopulation/',wam.cam[[file]]), header = T)
+  tmp <- read.table(paste0('./MSMC2_Data/CrossPopulation/',wam.cam[[file]]), header = T)
   if (file == 1) { tmp$set <- c(rep('1',nrow(tmp)))}
   else if (file == 2) {tmp$set <- c(rep('2',nrow(tmp)))}
   else if (file == 3) {tmp$set <- c(rep('3',nrow(tmp)))}
@@ -277,14 +152,13 @@ ggplot(data=wam.cam.combined[which(wam.cam.combined$time <= 3e07),], aes(x=(time
   facet_zoom(xlim = c(0,2e06))
   #facet_zoom(xlim = c(3.3e05,5e05))
 
-
 ## ==> for WAM-CAM: start of separation between 90,000 and 500,000 generations ago
 
 ## AM-ATL:
-am.atl <- list.files('~/Dropbox/Projects/Lbraz_genome_paper/Lbraziliensis/3_LBRA/2_Popgen/2_Popgen_with_PER186/10_G-PhoCS/4_MSMC2/3_msmc2_output/3_AllSNPs_EM500/CrossPopulation/', pattern = 'combined_AM-ATL')
+am.atl <- list.files('./MSMC2_Data/CrossPopulation/', pattern = 'combined_AM-ATL')
 am.atl.combined <- list()
 for ( file in 1:length(am.atl)){
-  tmp <- read.table(paste0('~/Dropbox/Projects/Lbraz_genome_paper/Lbraziliensis/3_LBRA/2_Popgen/2_Popgen_with_PER186/10_G-PhoCS/4_MSMC2/3_msmc2_output/3_AllSNPs_EM500/CrossPopulation/',am.atl[[file]]), header = T)
+  tmp <- read.table(paste0('./MSMC2_Data/CrossPopulation/',am.atl[[file]]), header = T)
   if (file == 1) { tmp$set <- c(rep('1',nrow(tmp)))}
   else if (file == 2) {tmp$set <- c(rep('2',nrow(tmp)))}
   else if (file == 3) {tmp$set <- c(rep('3',nrow(tmp)))}
